4a0c506d700970078c55934bb64017a808c0a898
Roman Rader <antigluk@gmail.com>
2012-11-02 18:51:53 +0200

    Tests skeleton for travis

---
+++ tests/test_lexer.py
@@ -0,0 +1,19 @@
+from nose.tools import assert_true
+
+class TestLexer(object):
+    @classmethod
+    def setup_class(klass):
+        pass
+
+    @classmethod
+    def teardown_class(klass):
+        pass
+
+    def setUp(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_r(self):
+        assert_true(True)
